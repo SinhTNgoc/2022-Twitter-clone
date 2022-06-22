78be6a74bc09e31c1ef5b91b00589b1bcf459414
SinhTNgoc <tranngocsinh2211@gmail.com>
2022-06-22 09:26:32 +0700

    Config Image Linkt

---
+++ components/Sidebar.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* Logo */}
+      <div>
+        <Image
+          src="https://br.atsit.in/vi/wp-content/uploads/2021/11/twitter-xem-xet-nguoi-dung-ghi-nhat-ky-loi-ngoai-iphone.png"
+          width={50}
+          height={50}
+          alt="logo"
+        ></Image>
+      </div>
+      {/* menu */}
+      {/* button */}
+    </div>
+  );
+};
+
+export default Sidebar;
